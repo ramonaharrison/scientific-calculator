f8c8e137b6c11948f7f29fc59389133a87d61810
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-05-12 11:51:01 -0400

    Initial commit

---
+++ app/src/main/java/nyc/c4q/ramonaharrison/scientificcalculator/Calculate.java
@@ -0,0 +1,13 @@
+package nyc.c4q.ramonaharrison.scientificcalculator;
+
+/**
+ * Created by c4q-anthony-mcbride on 5/5/15.
+ */
+public abstract class Calculate {
+    private String unParsed;
+
+    public Calculate(String unParsed){
+        this.unParsed = unParsed;
+    }
+
+}
